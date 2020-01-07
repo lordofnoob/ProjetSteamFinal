3e75756c52900e1d5022e61234c1983196e66573
Quentin Denois <Quentin.denois@hotmail.fr>
2020-01-07 19:10:34 +0100

    LineRenderer

---
+++ Assets/Scripts/Mb_Connection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mb_Connection : MonoBehaviour
+{
+    private Collider2D collider;
+    private LineRenderer lr;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr.useWorldSpace = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetStartLinePosition(Vector3 startPos)
+    {
+        lr.SetPosition(0, startPos);
+        CenteredCollider();
+    }
+
+    public void SetEndLinePosition(Vector3 endPos)
+    {
+        lr.SetPosition(lr.positionCount - 1, endPos);
+        CenteredCollider();
+    }
+
+    public void CenteredCollider()
+    {
+        collider.transform.position = Vector3.Lerp(lr.GetPosition(0), lr.GetPosition(lr.positionCount - 1), 0.5f);
+    }
+}
---
+++ Assets/Scripts/Mb_Terminal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mb_Terminal : MonoBehaviour
+{
+    public Sc_Terminal scriptableTerminal;
+
+    public Transform inputNodesContainer;
+    public Transform ouputNodesContainer;
+
+    public List<Mb_Node> inputNodes = new List<Mb_Node>();
+    public List<Mb_Node> outputNodes = new List<Mb_Node>();
+
+
+    private void Awake()
+    {
+        inputNodes.AddRange(inputNodesContainer.GetComponentsInChildren<Mb_Node>());
+        outputNodes.AddRange(ouputNodesContainer.GetComponentsInChildren<Mb_Node>());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
