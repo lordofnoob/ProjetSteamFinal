6d39f71d70f0b446c367411c58118cf82d13a339
pieR <p.Soriot@rubika-edu.com>
2020-01-07 17:03:55 +0100

    opla les managers

---
+++ Assets/Scripts/Ma_GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ma_GameManager : MonoBehaviour
+{
+    public static Ma_GameManager instance;
+    public int redRessource, blueRessource, greenRessource;
+
+
+    private void Start()
+    {
+        instance = this;
+        blueRessource = Ma_LevelManager.instance.levelParameters.baseBlueRessource;
+        redRessource = Ma_LevelManager.instance.levelParameters.baseRedRessource;
+        greenRessource = Ma_LevelManager.instance.levelParameters.baseGreenRessource;
+    }
+}
---
+++ Assets/Scripts/Ma_LevelManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ma_LevelManager : MonoBehaviour
+{
+    public Sc_LevelParameters levelParameters;
+    public static Ma_LevelManager instance;
+    [HideInInspector] public float[] turnDuration;
+    bool counting;
+    float timeModifer =1;
+    float timeToCount;
+    int currentTimeIndex =0;
+   
+
+    private void Start()
+    {
+        instance = this;
+
+        turnDuration = levelParameters.bootTimes;
+        //setup du premier temps pour faire des actions
+        timeToCount = turnDuration[0];
+    }
+
+    void Update()
+    {
+        Counting();
+    }
+
+    void Counting()
+    {
+        timeToCount -= Time.deltaTime * timeModifer;
+        Ma_UiManager.instance.UpdateTimeDisplaySpot(Mathf.RoundToInt(timeToCount), turnDuration[currentTimeIndex+1]);
+        if (timeToCount<=0)
+        {
+            currentTimeIndex += 1;
+            timeToCount = turnDuration[currentTimeIndex];
+        }
+    }
+
+    public void ModifyTimeModifier(float modifierToAdd)
+    {
+        timeModifer += modifierToAdd;
+    }
+
+    public void ResetTimeModifier()
+    {
+        timeModifer = 1;
+    }
+
+    public void PauseTime()
+    {
+        timeModifer = 0;
+    }
+
+}
