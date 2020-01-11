a13b668d6814dc4cb95aeaffc823f437096b7421
pieR <p.Soriot@rubika-edu.com>
2020-01-11 19:16:53 +0100

    wola

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public static Gamemanager instance;
+    [SerializeField] Sc_LevelParameters levelParameters;
+    float moneyStolen;
+    float timeRemaining;
+    bool isPause;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        timeRemaining = levelParameters.timeToDoTheLevel;
+    }
+
+       
+
+    public void CheckItemToGet(Mb_Item itemStolen)
+    {
+        if (itemStolen==levelParameters.itemToGet1)
+        {
+
+        }
+        else if (itemStolen == levelParameters.itemToGet2)
+        {
+
+        }
+    }
+
+    public void AddMoney(int moneyToAdd)
+    {
+        moneyStolen += moneyToAdd;
+    }
+
+    public void CheckMoney()
+    {
+        if (moneyStolen> levelParameters.moneyToSteal)
+        {
+
+        }
+    }
+
+    private void Update()
+    {
+        if (isPause == false)
+            DecreaseTimer();
+
+    }
+
+    void DecreaseTimer()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            Ma_UiManager.instance.UpdateTimeRemainingText(timeRemaining);
+        }
+        else
+            EndGame();
+
+    }
+
+    void StartGame()
+    {
+
+    }
+
+    void EndGame()
+    {
+
+    }
+
+    public void PauseTimer()
+    {
+        isPause = true;
+    }
+
+    public void ResumeGame()
+    {
+        isPause = false;
+    }
+
+}
---
+++ Assets/Scripts/Mb_StealTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mb_StealTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Mb_Item itemEntering = other.GetComponent<Mb_Item>();
+        if (itemEntering != null)
+            CheckItem(itemEntering);
+    }
+
+    void CheckItem(Mb_Item itemToCheck)
+    {
+        Gamemanager.instance.AddMoney(itemToCheck.itemValue);
+        Gamemanager.instance.CheckItemToGet(itemToCheck);
+    }
+}
