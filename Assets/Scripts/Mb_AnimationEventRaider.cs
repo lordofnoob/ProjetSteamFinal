dcbaa0b72721fd0d14e60aecc44cfcaa71ad929b
pieR <p.Soriot@rubika-edu.com>
2020-01-12 16:20:00 +0100

    aiznfz

---
+++ Assets/Scripts/Mb_AnimationEventRaider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mb_AnimationEventRaider : MonoBehaviour
+{
+    [SerializeField] Mb_PlayerControler controledPlayer;
+
+    public void SetCanMoveTrueTrue()
+    {
+        controledPlayer.SetCanMove(true);
+    }
+}
