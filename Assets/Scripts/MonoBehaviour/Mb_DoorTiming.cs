24b40d39a4bd8029dfc98c7f718afcd7e5b94678
pieR <p.Soriot@rubika-edu.com>
2020-01-21 18:02:14 +0100

    aeazeaze
    azeaze

--- Assets/Scripts/MonoBehaviour/Mb_DoorTiming.cs
+++ Assets/Scripts/MonoBehaviour/Mb_DoorTiming.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Mb_DoorTiming : Mb_Door
 {
     [SerializeField] float timeBeforeClose;
+    [SerializeField] Animator animToTrigger;
     float timing = 0;
     bool counting = false;
 
@@ -17,6 +18,10 @@ public class Mb_DoorTiming : Mb_Door
             DoThings();
             timing = 0;
         }
+
+        if (timing > timeBeforeClose - 5)
+            animToTrigger.SetTrigger("DoThings");
+
     }
 
     public override void DoThings()
