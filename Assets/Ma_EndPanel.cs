af50fee18858b310eca5ab7505c48f3d65cf1007
KGales <galeskillian@gmail.com>
2020-01-24 17:14:41 +0100

    Work ld lvl01

---
+++ Assets/Ma_EndPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ma_EndPanel : MonoBehaviour
+{
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ResartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
